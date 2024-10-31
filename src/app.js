be745b766060b2662c0ff528b414b1ce8f29a9c9
Virendra Sirture <virendrasirture05@gmail.com>
2024-10-31 11:31:16 +0530

    multiple route-handlers

--- src/app.js
+++ src/app.js
@@ -1,24 +1,46 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
+app.use(
+  "/user",
+  (req, res,next) => {
+    console.log("handling respones 1");
+    res.send("response 1");
+    next()
+  },
+  (req, res,next) => {
+    console.log("handling response 2");
+    res.send("response 2");
+    next()
+  },
+  (req,res,next)=>{
+    console.log('handline response 3');
+    // res.send('response 3');
+    next()
+  }
+);
 
-app.get('/user',(req,res)=>{
-    res.send({firstName : "Virendra", lastName : "Sirture"})
-})
+// app.get('/user',(req,res)=>{
+//     res.send({firstName : "Virendra", lastName : "Sirture"})
+// })
+// app.get('/user/:id',(req,res)=>{
+//     // console.log(req.query);
+//     console.log(req.params.id);
+//     res.send('done');
+// })
+// app.post("/user",(req,res)=>{
+//     res.send('data created succesfully')
+// })
+// app.delete('/user',(req,res)=>{
+//     res.send('data deleted');
+// })
+// app.put("/user",(req,res)=>{
+//     res.send('data has put');
+// })
+// app.use('/test',(req,res)=>{
+//     res.send('text')
+// })
 
-app.post("/user",(req,res)=>{
-    res.send('data created succesfully')
-})
-app.delete('/user',(req,res)=>{
-    res.send('data deleted');
-})
-app.put("/user",(req,res)=>{
-    res.send('data has put');
-})
-
-app.use('/test',(req,res)=>{
-    res.send('text')
-})
-app.listen(3000,(req,res)=>{
-    console.log('server is started');
-})
+app.listen(3000, (req, res) => {
+  console.log("server is started");
+});
